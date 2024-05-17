0b6be5f2d925e94ca58c3b95ba8e690a54e0b2ca
Anvidneo <Botero1400@gmail.com>
2024-05-17 16:18:28 -0500

    [Feature]: First commit 🧩

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Providers;
+
+use App\Core\Contracts\ExternalAdapterContract;
+use App\Core\Repositories\PermissionRepository;
+use App\Core\Repositories\ProductRepository;
+use App\Core\Repositories\CategoryRepository;
+use App\Core\Repositories\RolRepository;
+use App\Core\Repositories\UserRepository;
+use App\Infraestructure\Adapters\ExternalServiceAdapter;
+use App\Infraestructure\Repositories\EloquentPermissionRepository;
+use App\Infraestructure\Repositories\EloquentProductRepository;
+use App\Infraestructure\Repositories\EloquentCategoryRepository;
+use App\Infraestructure\Repositories\EloquentRolRepository;
+use App\Infraestructure\Repositories\EloquentUserRepository;
+use App\Infraestructure\Services\ExternalService;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(UserRepository::class, EloquentUserRepository::class);
+        $this->app->bind(RolRepository::class, EloquentRolRepository::class);
+        $this->app->bind(PermissionRepository::class, EloquentPermissionRepository::class);
+        $this->app->bind(ProductRepository::class, EloquentProductRepository::class);
+        $this->app->bind(CategoryRepository::class, EloquentCategoryRepository::class);
+        $this->app->bind(ExternalAdapterContract::class, ExternalServiceAdapter::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('product')->group(function () {
+    Route::get('', [ProductController::class, 'index']);
+    Route::get('/{id}', [ProductController::class, 'find']);
+    Route::post('', [ProductController::class, 'store']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
+
